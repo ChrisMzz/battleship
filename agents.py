7405af98c496e4828410d1c2a32b38f01163bc4d
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2023-12-27 20:50:08 +0100

    fixed negatives appearing in distribution

--- agents.py
+++ agents.py
@@ -63,7 +63,7 @@ class Player:
             
             self.boats_shuffle = [parent1.boats_shuffle, parent2.boats_shuffle][np.random.randint(0,2)]
             has_mutation = np.random.rand()
-            if has_mutation < mu_k: m_kernel = np.array(np.random.randint(-2,3, size=(3,3)), dtype=np.float64)
+            if has_mutation < mu_k: m_kernel = np.array(np.random.randint(0,3, size=(3,3)), dtype=np.float64)
             else: m_kernel = np.zeros((3,3))
             if np.sum(m_kernel) != 0: 
                 m_kernel /= np.sum(m_kernel)
@@ -117,7 +117,8 @@ class Player:
                 
                 
     def guess(self, other:Player):
-        dstrb = ( self.boat_gss_dstrb + convolve(self.guess_grid==2, self.kernel) )*(self.guess_grid==0)
+        dstrb = ( self.boat_gss_dstrb + convolve((self.guess_grid==2), self.kernel) )*(self.guess_grid==0)
+        dstrb *= dstrb>0
         dstrb /= np.sum(dstrb)
         if np.isnan(dstrb).any(): 
             dstrb = (np.ones((self.grid_size,self.grid_size))*(self.guess_grid==0))
@@ -129,7 +130,7 @@ class Player:
         return (other.boat_grid[line,column] == 1), (line, column)
     
     def compute_consecutives(self): 
-        return label(np.array(self.hits))[1]
+        return label(convolve(np.array(self.hits), [1,1,1])>0)[1]
             
 
 def game(black:Player, white:Player):
@@ -147,11 +148,15 @@ def game(black:Player, white:Player):
                                                               [1,4,1],
                                                               [0,1,0]])/8)
             white.boat_gss_dstrb += hit_grid_a
-            white.boat_gss_dstrb *= (white.boat_gss_dstrb>0)
+            white.boat_gss_dstrb *= white.boat_gss_dstrb>0
+            if np.sum(white.boat_plc_dstrb) == 0: white.boat_plc_dstrb = np.ones((white.grid_size,white.grid_size))
             white.boat_gss_dstrb /= np.sum(white.boat_gss_dstrb)
+            # print(np.sum(white.boat_gss_dstrb<0))
             black.boat_plc_dstrb -= hit_grid_d
-            black.boat_plc_dstrb *= (black.boat_plc_dstrb>0)
+            black.boat_plc_dstrb *= black.boat_plc_dstrb>0
+            if np.sum(black.boat_plc_dstrb) == 0: black.boat_plc_dstrb = np.ones((black.grid_size,black.grid_size))
             black.boat_plc_dstrb /= np.sum(black.boat_plc_dstrb)
+            # print(np.sum(black.boat_plc_dstrb<0))
         else:
             white.hits.append(0)
             hit_grid = np.zeros((white.grid_size, white.grid_size))
@@ -160,8 +165,10 @@ def game(black:Player, white:Player):
                                                               [1,4,1],
                                                               [0,1,0]])/8)/4
             white.boat_gss_dstrb -= hit_grid_a
-            white.boat_gss_dstrb *= (white.boat_gss_dstrb>0)
+            white.boat_gss_dstrb *= white.boat_gss_dstrb>0
+            if np.sum(white.boat_plc_dstrb) == 0: white.boat_plc_dstrb = np.ones((white.grid_size,white.grid_size))
             white.boat_gss_dstrb /= np.sum(white.boat_gss_dstrb)
+            # print(np.sum(white.boat_gss_dstrb<0))
         grid.append(np.c_[black.boat_grid+2*white.guess_grid+1, np.zeros((black.grid_size,black.grid_size)), white.boat_grid+2*black.guess_grid+1])
         if np.sum(white.guess_grid==2) == np.sum(black.boat_grid): return white, np.array(grid)
         
@@ -177,11 +184,15 @@ def game(black:Player, white:Player):
                                                               [1,4,1],
                                                               [0,1,0]])/8)
             black.boat_gss_dstrb += hit_grid_a
-            black.boat_gss_dstrb *= (black.boat_gss_dstrb>0)
+            black.boat_gss_dstrb *= black.boat_gss_dstrb>0
+            if np.sum(black.boat_gss_dstrb) == 0: black.boat_gss_dstrb = np.ones((black.grid_size,black.grid_size))
             black.boat_gss_dstrb /= np.sum(black.boat_gss_dstrb)
+            # print(np.sum(black.boat_gss_dstrb<0))
             white.boat_plc_dstrb -= hit_grid_d
-            white.boat_plc_dstrb *= (white.boat_plc_dstrb>0)
+            white.boat_plc_dstrb *= white.boat_plc_dstrb>0
+            if np.sum(white.boat_plc_dstrb) == 0: white.boat_plc_dstrb = np.ones((white.grid_size,white.grid_size))
             white.boat_plc_dstrb /= np.sum(white.boat_plc_dstrb)
+            # print(np.sum(white.boat_plc_dstrb<0))
         else:
             black.hits.append(0)
             hit_grid = np.zeros((black.grid_size, black.grid_size))
@@ -190,8 +201,10 @@ def game(black:Player, white:Player):
                                                               [1,4,1],
                                                               [0,1,0]])/8)/4
             black.boat_gss_dstrb -= hit_grid_a
-            black.boat_gss_dstrb *= (black.boat_gss_dstrb>0)
+            black.boat_gss_dstrb *= black.boat_gss_dstrb>0
+            if np.sum(black.boat_gss_dstrb) == 0: black.boat_gss_dstrb = np.ones((black.grid_size,black.grid_size))
             black.boat_gss_dstrb /= np.sum(black.boat_gss_dstrb)
+            # print(np.sum(black.boat_gss_dstrb<0))
         grid.append(np.c_[black.boat_grid+2*white.guess_grid+1, np.zeros((black.grid_size,black.grid_size)), white.boat_grid+2*black.guess_grid+1])
         if np.sum(black.guess_grid==2) == np.sum(white.boat_grid): return black, np.array(grid)
         
@@ -205,7 +218,7 @@ if __name__=='__main__':
     pop_size = 100 # multiple of 4
 
 
-    next_generation = [Player(boats=[3,4,5,5,8,10], grid_size=20) for _ in range(pop_size)]
+    next_generation = [Player(boats=[2,3,3,4,5], grid_size=10) for _ in range(pop_size)]
     #pdb.set_trace()
     
     progress_bar = tqdm.trange(10)
@@ -215,16 +228,20 @@ if __name__=='__main__':
         winners = [winners_and_games[i][0] for i in range(len(winners_and_games))]
         games = [winners_and_games[i][1] for i in range(len(winners_and_games))]
         progress_bar.set_postfix({"average_length":np.mean([len(games[i]) for i in range(len(games))])})
+        progress_bar.set_postfix({"average_consecs":np.mean([winner.compute_consecutives() for winner in winners])})
+        
         #pdb.set_trace()
         next_generation = []
         while len(next_generation) < pop_size:
             np.random.shuffle(winners)
             for black, white in zip(winners[:pop_size//4+1], winners[pop_size//4:]):
-                if black.compute_consecutives() + white.compute_consecutives() > 1.5*(len(black.boats) + len(white.boats)): continue
+                ignore = np.random.choice([False, True], p=[0.995,0.005])
+                if not ignore and (black.compute_consecutives() + white.compute_consecutives() > 3*(len(black.boats) + len(white.boats))): 
+                    continue
                 next_generation.append(Player(black, white))
         next_generation = np.random.choice(next_generation, pop_size, replace=False)
     
-    imsave("replay_AGAIN.tif", game(*next_generation[:2])[1])
+    imsave("replay_10x10_10.tif", game(*next_generation[:2])[1])
     
     #pdb.set_trace()
 
